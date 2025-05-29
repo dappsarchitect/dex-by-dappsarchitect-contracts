29d58a67315d596c3db187f941ff00caee061217
Raymond Yeung <raymond.yeung@email.com>
2025-05-30 00:53:57 +0800

    5. Added Transfer Tokens.

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,6 +9,8 @@ contract Token {
 
     mapping(address => uint256) public balanceOf;
 
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -21,5 +23,16 @@ contract Token {
         totalSupply = _totalSupply * (10 ** decimals);
 
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value, "Token: Insufficient Funds");
+        require(_to != address(0), "Token: Recipient Is Address 0");
+
+        balanceOf[msg.sender] -= _value; // Deduct tokens from sender
+        balanceOf[_to] += _value; // Credit tokens to recipient
+            
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -8,33 +8,79 @@ const unitFixer = (n) => {
 }
 
 describe("Token", () => {
-    const NAME = "Dapps Architect"
-    const SYMBOL = "DACT"
-    const DECIMALS = 18
-    const TOTAL_SUPPLY = unitFixer(1000000)
 
-    it("has correct name", async () => {
-        const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.name()).to.equal(NAME) // fetches name from the blockchain and tests it
+    describe("Deployment", () => {
+        const NAME = "Dapps Architect";
+        const SYMBOL = "DACT";
+        const DECIMALS = 18;
+        const TOTAL_SUPPLY = unitFixer(1000000);
+
+        it("has correct name", async () => {
+            const { token } = await loadFixture(deployTokenFixture);
+            expect(await token.name()).to.equal(NAME); // fetches name from the blockchain and tests it
+        });
+
+        it("has correct symbol", async () => {
+            const { token } = await loadFixture(deployTokenFixture);
+            expect(await token.symbol()).to.equal(SYMBOL); // fetches symbol from the blockchain and tests it
+        });
+
+        it("has correct decimals", async () => {
+            const { token } = await loadFixture(deployTokenFixture);
+            expect(await token.decimals()).to.equal(DECIMALS); // fetches decimals from the blockchain and tests it
+        });
+
+        it("has correct total supply", async () => {
+            const { token } = await loadFixture(deployTokenFixture);
+            expect(await token.totalSupply()).to.equal(TOTAL_SUPPLY); // fetches total supply from the blockchain and tests it to be 1000000000000000000000000
+        });
+
+        it("assigns token total supply to the contract deployer", async () => {
+            const { token, deployer } = await loadFixture(deployTokenFixture);
+            expect(await token.balanceOf(deployer.address)).to.equal(TOTAL_SUPPLY);
+        });
     })
 
-    it("has correct symbol", async () => {
-        const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.symbol()).to.equal(SYMBOL) // fetches symbol from the blockchain and tests it
-    })
+    describe("Transferring Tokens", () => {
 
-    it("has correct decimals", async () => {
-        const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.decimals()).to.equal(DECIMALS) // fetches decimals from the blockchain and tests it
-    })
+        describe("Success", () => {
+            it("transfers token balances", async () => {
+                const { token, deployer, recipient } = await loadFixture(deployTokenFixture);
 
-    it("has correct total supply", async () => {
-        const { token } = await loadFixture(deployTokenFixture)
-        expect(await token.totalSupply()).to.equal(TOTAL_SUPPLY) // fetches total supply from the blockchain and tests it to be 1000000000000000000000000
-    })
+                const transaction = await token.connect(deployer).transfer(recipient.address, unitFixer(500000))
+                await transaction.wait()
 
-    it("assigns token total supply to the contract deployer", async () => {
-        const { token, deployer } = await loadFixture(deployTokenFixture)
-        expect(await token.balanceOf(deployer.address)).to.equal(TOTAL_SUPPLY)
+                expect(await token.balanceOf(deployer.address)).to.equal(unitFixer(500000));
+                expect(await token.balanceOf(recipient.address)).to.equal(unitFixer(500000))
+            })
+
+            it("emits Transfer event", async () => {
+                const { token, deployer, recipient } = await loadFixture(deployTokenFixture);
+
+                const transaction = await token.connect(deployer).transfer(recipient.address, unitFixer(500000))
+                await transaction.wait()
+
+                await expect(transaction).to.emit(token, "Transfer")
+                    .withArgs(deployer.address, recipient.address, unitFixer(500000))
+            })
+        })
+
+        describe("Failure", () => {
+            it("rejects insufficient balance", async () => {
+                const { token, deployer, recipient } = await loadFixture(deployTokenFixture);
+                const INVALID_AMOUNT = unitFixer(2000000)
+                const ERROR = "Token: Insufficient Funds"
+
+                await expect(token.connect(deployer).transfer(recipient.address, INVALID_AMOUNT)).to.be.revertedWith(ERROR)
+            })
+
+            it("rejects invalid recipient", async () => {
+                const { token, deployer, recipient } = await loadFixture(deployTokenFixture);
+                const INVALID_ADDRESS = "0x0000000000000000000000000000000000000000"
+                const ERROR = "Token: Recipient Is Address 0"
+
+                await expect(token.connect(deployer).transfer(INVALID_ADDRESS, unitFixer(500000))).to.be.revertedWith(ERROR)
+            })
+        })
     })
 })
